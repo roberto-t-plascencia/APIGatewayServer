5e40d36763b2d797365eb32b0868f6bff11ca4d0
roberto-t-plascencia <64867422+roberto-t-plascencia@users.noreply.github.com>
2020-11-23 13:28:20 -0800

    Create LoginController.cs

---
+++ APIGatewayServer/Controllers/LoginController.cs
@@ -0,0 +1,73 @@
+using Microservice.gateway.api.Model;
+using Microservice.gateway.api.Repository;
+using Microservice.gateway.api.VM;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Microservice.gateway.api.Controllers
+{
+    //[Authorize] //for Active Directory integration
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IUserRepository _userRepository;
+
+        public LoginController(IUserRepository repo)
+        {
+            _userRepository = repo;
+        }
+
+        //[Authorize] //for Active Directory integration
+        [Route("Add")]
+        [HttpPost]
+        public async Task<ActionResult> AddUser([FromBody]Register user)
+        {
+            string returnValue = await _userRepository.Add(user);
+            if (string.IsNullOrEmpty(returnValue))
+                return StatusCode(403);
+            else
+                return Ok(200);
+        }
+
+        //[Authorize] //for AD integration
+        [Route("Login")]
+        [HttpPost]
+        public async Task<ActionResult> UserLogin(Login login)
+        {
+            Register user = await _userRepository.Login(login);
+            if (user == null)
+                return StatusCode(403);
+            else
+                return Ok(200);
+        }
+
+        // [Authorize] //for AD integration
+        [Route("Remove")]
+        [HttpPost]
+        public async Task<ActionResult> Remove(string username)
+        {
+            var returnValue = await _userRepository.Remove(username);
+            if (string.IsNullOrEmpty(returnValue))
+                return StatusCode(403);
+            else
+                return Ok(200);
+        }
+
+        //[Authorize] //for AD integration
+        [Route("GetUsers")]
+        [HttpGet]
+        public IEnumerable<Register> GetAll()
+        {
+            return _userRepository.GetUsers();
+        }
+
+    }
+}
