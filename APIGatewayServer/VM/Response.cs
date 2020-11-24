cf0a59608de592338da0ea702f634046ba47a070
roberto-t-plascencia <64867422+roberto-t-plascencia@users.noreply.github.com>
2020-11-23 16:26:16 -0800

    Add files via upload

---
+++ APIGatewayServer/VM/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.gateway.api.VM
+{
+    public class Response
+    {
+        public string Status { set; get; }
+        public string Message { set; get; }
+    }
+}
