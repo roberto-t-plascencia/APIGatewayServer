4b3c948cbd8610c4df1e4f4d03a9b2dee6bd2c16
roberto-t-plascencia <64867422+roberto-t-plascencia@users.noreply.github.com>
2020-11-23 16:22:31 -0800

    Add files via upload

---
+++ APIGatewayServer/Model/Register.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.gateway.api.Model
+{
+    public class Register
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string City { get; set; }
+        public int PhoneNumber { get; set; }
+
+    }
+}
---
+++ APIGatewayServer/Model/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.gateway.api.Model
+{
+    public class UserLogin
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string EmployeeName { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+    }
+}
