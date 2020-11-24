6a486b4d6b769404b69e428524fa2f57a2f9dd26
roberto-t-plascencia <64867422+roberto-t-plascencia@users.noreply.github.com>
2020-11-23 16:20:57 -0800

    Add files via upload

---
+++ APIGatewayServer/DbContexts/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microservice.gateway.api.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.gateway.api.DbContexts
+{
+	public interface IApplicationDbContext
+	{
+		//DbSet<Order> Orders { get; set; }
+		Dictionary<string, Order>  Orders { get; set; }
+		Task<int> SaveChanges();
+
+	}
+}
